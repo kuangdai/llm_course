965679b481412607eeb05e4db4bebb8039d2aae9
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-10-29 12:23:37 +0000

    Update 03_MyLLMServer.py

--- 03_MyLLMServer.py
+++ 03_MyLLMServer.py
@@ -1,3 +1,6 @@
+import faiss
+import numpy as np
+import pandas as pd
 import torch
 from flask import Flask, request, jsonify
 from huggingface_hub import login
@@ -6,6 +9,9 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 # Initialize Flask app
 app = Flask(__name__)
 
+#########
+# Model #
+#########
 # Login to Huggingface
 hf_access_key = "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"  # noqa
 login(hf_access_key)
@@ -29,23 +35,31 @@ model = AutoModelForCausalLM.from_pretrained(
     quantization_config=quantization_config
 )
 
+###############
+# Poetry data #
+###############
 
-# Function to compute embedding
-def compute_embedding(text):
-    inputs = tokenizer([text], return_tensors="pt", padding=True)
-    with torch.no_grad():
-        outputs = model(**inputs, output_hidden_states=True)
-    hidden_states = outputs.hidden_states[-1]
-    attention_mask = inputs['attention_mask'].unsqueeze(-1).expand(hidden_states.size())
-    masked_hidden_states = hidden_states * attention_mask
-    sum_embeddings = masked_hidden_states.sum(dim=1)
-    non_pad_tokens = attention_mask.sum(dim=1)
-    embedding = sum_embeddings / non_pad_tokens.clamp(min=1e-9)
-    return embedding[0].cpu().numpy()
+# Load the .npz file with allow_pickle=True
+loaded_npz = np.load('data/input/poetry_data_clean.npz', allow_pickle=True)
+
+# Reconstruct the DataFrame using the saved data and columns
+df = pd.DataFrame(loaded_npz['df'], columns=loaded_npz['columns'])
 
 
-# Function to generate only new tokens as text
-def generate_text(text, temperature=0.1, max_new_tokens=25):
+def formatted(idx, include_tags=False):
+    """ format poems """
+    it = df.iloc[idx]
+    res = f'{it["Title"]}\n{it["Poet"]}\n--------\n\n{it["Poem"]}'
+    if it["Tags"] and include_tags:
+        res += f'\n\n--------\nNotes: {it["Tags"]}'
+    return res
+
+
+###################
+# Text generation #
+###################
+def generate_kernel(text, temperature=0.1, max_new_tokens=25):
+    """Function to generate only new tokens as text"""
     inputs = tokenizer([text], return_tensors="pt", padding=True)
     with torch.no_grad():
         outputs = model.generate(
@@ -57,55 +71,82 @@ def generate_text(text, temperature=0.1, max_new_tokens=25):
         )
     # Decode only the newly generated tokens (excluding input text tokens)
     input_length = inputs['input_ids'].shape[1]
-    generated_text = tokenizer.decode(outputs[0][input_length:], skip_special_tokens=True)
-    return generated_text
+    return tokenizer.decode(outputs[0][input_length:], skip_special_tokens=True)
 
 
-# Route for computing embeddings
-@app.route("/embed_llama3", methods=["POST"])
-def embed_llama3():
-    data = request.json
-    text = data.get("text", "")
-
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
-
-    # Compute and return embedding
-    embedding = compute_embedding(text)
-    return jsonify({"embedding": embedding.tolist()})
-
-
-# Route for generating text
-@app.route("/generate_llama3", methods=["POST"])
-def generate_llama3():
+@app.route("/generate", methods=["POST"])
+def generate():
+    """Route to generate"""
     data = request.json
     text = data.get("text", "")
     temperature = data.get("temperature", 0.1)
     max_new_tokens = data.get("max_new_tokens", 50)
-
     if not text:
         return jsonify({"error": "No text provided"}), 400
 
     # Generate and return text
-    generated_text = generate_text(text, temperature=temperature, max_new_tokens=max_new_tokens)
-    return jsonify({"generated_text": generated_text})
+    generated_text = generate_kernel(text, temperature=temperature, max_new_tokens=max_new_tokens)
+    return jsonify({"generated": generated_text})
 
-
-# To test the server for embedding computation:
-"""
-curl -X POST http://localhost:7777/embed_llama3 \
-     -H "Content-Type: application/json" \
-     -d '{"text": "Sample text to compute embedding"}'
-"""
 
 # To test the server for text generation:
 """
-curl -X POST http://localhost:7777/generate_llama3 \
+curl -X POST http://localhost:7777/generate \
      -H "Content-Type: application/json" \
      -d '{
-           "text": "Sample text to generate inference output",
+           "text": "Hello. How are you today?",
            "temperature": 0.7,
            "max_new_tokens": 50
+         }'
+"""
+
+#######################
+# Similarity retrival #
+#######################
+# Load Faiss index
+faiss_index = faiss.read_index("data/knowledge_bases/poetry_faiss.index")
+
+
+def retrieve_faiss_kernel(text, k):
+    """Function to retrieve from faiss index"""
+    # Compute embedding
+    inputs = tokenizer([text], return_tensors="pt", padding=True)
+    with torch.no_grad():
+        outputs = model(**inputs, output_hidden_states=True)
+    hidden_states = outputs.hidden_states[-1]
+    attention_mask = inputs['attention_mask'].unsqueeze(-1).expand(hidden_states.size())
+    masked_hidden_states = hidden_states * attention_mask
+    sum_embeddings = masked_hidden_states.sum(dim=1)
+    non_pad_tokens = attention_mask.sum(dim=1)
+    embedding = sum_embeddings / non_pad_tokens.clamp(min=1e-9)
+
+    # Retrieve indices
+    distances, indices = faiss_index.search(embedding.cpu().numpy(), k=k)
+
+    # Return text
+    return [formatted(i) for i in indices]
+
+
+@app.route("/retrieve_faiss_llama3", methods=["POST"])
+def retrieve_faiss():
+    """Route to retrieve from faiss index"""
+    data = request.json
+    text = data.get("text", "")
+    k = data.get("k", 1)
+    if not text:
+        return jsonify({"error": "No text provided"}), 400
+
+    # Compute and return embedding
+    return jsonify({"retrieved": retrieve_faiss_kernel(text, k)})
+
+
+# To test the server for similarity retrival:
+"""
+curl -X POST http://localhost:7777/retrieve_faiss_llama3 \
+     -H "Content-Type: application/json" \
+     -d '{
+            "text": "I don't know how you were diverted\nYou were perverted, too",
+            "k": 1
          }'
 """
 
