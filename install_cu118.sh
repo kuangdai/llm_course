c5b2b6b62449669d958e471927f98d82d78f28d9
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-11-16 17:11:45 +0000

    Update install_cu118.sh

--- install_cu118.sh
+++ install_cu118.sh
@@ -1,5 +1,5 @@
 # Step 1: Create a Conda environment for the LLM course with Python 3.10
-conda create -n llm_course python=3.10 -y
+conda create -n llm_course python=3.9 -y
 
 # Step 2: Activate the Conda environment
 conda activate llm_course
@@ -11,7 +11,7 @@ pip install flask gdown jupyter matplotlib numpy pandas scipy spacy tqdm
 python -m spacy download en_core_web_sm
 
 # Step 5: Install PyTorch with audio and vision support for deep learning tasks
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch=2.4.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 # Step 6: Install Hugging Face libraries for working with language models and distributed training
 pip install huggingface_hub transformers accelerate
@@ -28,7 +28,7 @@ pip install langchain langchain_community langgraph
 # Step 10: Install vLLM, optimized for high-performance language model inference
 # Install vLLM with CUDA 11.8.
 export VLLM_VERSION=0.6.1.post1
-export PYTHON_VERSION=310
+export PYTHON_VERSION=309
 pip install https://github.com/vllm-project/vllm/releases/download/v${VLLM_VERSION}/vllm-${VLLM_VERSION}+cu118-cp${PYTHON_VERSION}-cp${PYTHON_VERSION}-manylinux1_x86_64.whl --extra-index-url https://download.pytorch.org/whl/cu118
 
 # Step 11: Install Localtunnel for public URL access (requires npm)
