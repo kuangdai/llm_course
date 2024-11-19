8325bfdfe9ec40d7aa21037f13075586603b94ce
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-11-19 11:19:40 +0000

    Update install_cu118.sh

--- install_cu118.sh
+++ install_cu118.sh
@@ -28,7 +28,7 @@ pip install langchain langchain_community langgraph
 # Step 10: Install vLLM, optimized for high-performance language model inference
 # Install vLLM with CUDA 11.8.
 export VLLM_VERSION=0.6.1.post1
-export PYTHON_VERSION=39
+export PYTHON_VERSION=310
 pip install https://github.com/vllm-project/vllm/releases/download/v${VLLM_VERSION}/vllm-${VLLM_VERSION}+cu118-cp${PYTHON_VERSION}-cp${PYTHON_VERSION}-manylinux1_x86_64.whl --extra-index-url https://download.pytorch.org/whl/cu118
 
 # Step 11: Install Localtunnel for public URL access (requires npm)
